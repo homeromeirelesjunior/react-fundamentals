4d00249629794e912fe7b5a4f156b112d566540f
homeromeirelesjunior <homeromeirelesjunior@gmail.com>
2021-03-10 01:10:17 +0000

    (09/03) Exercícios 05 e 06

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,16 +12,29 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+//const smallBox = <div className="box box--small" style={  { backgroundColor: 'lightblue', fontStyle: 'italic'} }>small lightblue box</div>
+//const mediumBox = <div className="box box--medium" style={  { backgroundColor: 'pink', fontStyle: 'italic', filter: 'brightnessa(70%)'} }> medium pink box</div>
+//const largeBox = <div className="box box--large" style={  { backgroundColor: 'orange', fontStyle: 'italic'} }>large orange box</div>
+
+function Box( {color = 'lightblue', style = {}, size = 'medium', ...props} ) {
+    const className = 'box box--' + size
+    return <div className={className} style={ {fontStyle: 'italic', backgroundColor: color, ...style} } {...props}></div>
+}
 
 function App() {
-  return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+    return (
+        <div>
+            {/*
+                Em JSX, estilos CSS são passados como objetos para o elemento.
+                O nome dos atributos em CSS, escritas em keab-case, tornam-se
+                os nomes das propriedades do objeto, mas ecritas em camelCase.
+                O valor das propriedades vai entre as aspas, caso não seja numérico.
+            */}
+            <div style={ { marginTop: '20px' , backgroundColor: 'blue' } }>Teste</div>
+            <Box size="small" id1="box1">Small LightBlue Box</Box>
+            <Box color="pink" id2="box2">Medium LightBlue Box</Box>
+            <Box size="large" color="orange" id="box3">Large LightBlue Box</Box>
+            <Box>Texto</Box>
     </div>
   )
 }
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 
+
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
@@ -17,21 +18,48 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 add the onSubmit handler to the <form> below
 
   // 🐨 make sure to associate the label to the input.
-  // to do so, set the value of 'htmlFor' prop of the label to the id of input
-  return (
-    <form>
-      <div>
-        <label>Username:</label>
-        <input type="text" />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+
+    // criando uma ref, um recurso do React para acessar conteúdo
+    // de elementos de formulários
+    const usernameE1 = React.useRef()
+
+
+
+      return (
+        <form onSubmit={handleSubmit}>
+            <div>
+           <label>Username:</label>
+           {/* Associando o ref usernameE1 ao input*/}
+           <input  ref={usernameE1} id="username" type="text" />
+             </div>
+              <button type="submit">Submit</button>
+        </form>
   )
+  
+      function handleSubmit(event) {
+        //const username = document.querySelector('#username').value
+        // Previne o recarregamento do formuáleario
+        event.preventDefault()
+
+        const username = usernameE1.current.value
+
+        onSubmitUsername(username)
+    }
+
+
 }
+
+
+
+
 
 function App() {
   const onSubmitUsername = username => alert(`You entered: ${username}`)
   return <UsernameForm onSubmitUsername={onSubmitUsername} />
 }
+
+//document.querySelector('form').addEventListener('submit', handleSubmit)
 
 export default App
